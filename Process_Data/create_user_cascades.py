7fca58ded2b70e4cc977bfb8263d83208a268eb0
imoutidi <imoutidi@gmail.com>
2023-04-22 16:31:49 +0200

    Merging comments and submission records in one pickle file.

---
+++ Process_Data/create_user_cascades.py
@@ -0,0 +1,36 @@
+import os
+
+from dateutil.relativedelta import relativedelta
+from Tool_Pack import tools
+import pandas as pd
+import json
+import csv
+import time
+import datetime
+
+
+def merge_submissions_and_comments():
+    comments_path = r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Storm_on_capitol\Comments\\"
+    all_submissions = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\\"
+                                        r"Storm_on_capitol\submission_records")
+    # convert submission file into a dictionary key = post id
+    # I don't iterate the submission_list file because the comments are
+    # divided in many pickle files.
+    submission_dict = dict()
+    for sub_record in all_submissions:
+        submission_dict[sub_record["id"]] = sub_record
+    # This will be a list of tuples
+    for idx, comment_file in enumerate(os.listdir(comments_path)):
+        print(idx)
+        merged_subs_and_comments = list()
+        current_comments = tools.load_pickle(comments_path + comment_file)
+        for submission_id, comment_attributes in current_comments.items():
+            merged_subs_and_comments.append((submission_dict[submission_id], comment_attributes))
+        tools.save_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\\"
+                          r"Storm_on_capitol\Merged_Submissions_Comments\subs_comments_"
+                          + str(idx), merged_subs_and_comments)
+
+
+if __name__ == "__main__":
+    merge_submissions_and_comments()
+    print()
--- Scrapping/Reddit_Scrappers/Scrape_Ukraine_Megathreads.py
+++ Scrapping/Reddit_Scrappers/Scrape_Ukraine_Megathreads.py
@@ -17,29 +17,37 @@ reddit = praw.Reddit(client_id="JA5vOF4gOhcjuGqdHU2Gcw", client_secret="3XT735o7
 
 def crawl_megathreads(start_post_id="12aw2q2"):
     posts_id_list = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\\"
-                                      r"Ukraine_War\all_megathreads_post_ids")
+                                      r"Ukraine_War\all_megathreads_post_ids_temp")
     # start_post_id = "v0v1yv"
-    posts_id_list.append(start_post_id)
     while start_post_id is not None:
         current_post = r_scrapper.get_post_with_id(start_post_id)
         if current_post is None:
-            print(f"Pushshift could not retrieve data for post {start_post_id}. Saving the post id list.")
+            print(f"Pushshift could not retrieve data for post {start_post_id}. Saving the post id list. \n"
+                  f"need to find the next post id manually.")
             tools.save_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\\"
                               r"Ukraine_War\all_megathreads_post_ids_temp", posts_id_list)
+            exit()
 
-        start_post_id = None
         post_text = current_post["selftext"]
+        post_utc_timestamp = current_post["created_utc"]
+        post_utc_date_str = current_post["utc_datetime_str"]
+        posts_id_list.append({"id": start_post_id, "str_date": post_utc_date_str, "timestamp": post_utc_timestamp})
+        start_post_id = None
         for token in post_text.split():
             if "war_in_ukraine_megathread_" in token:
                 print(token)
                 inner_token_list = token.split("/")
                 comment_index = inner_token_list.index("comments")
                 start_post_id = inner_token_list[comment_index + 1]
-                posts_id_list.append(start_post_id)
-        # time.sleep(0.5)
+        time.sleep(0.3)
     tools.save_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\\"
                       r"Ukraine_War\all_megathreads_post_ids", posts_id_list)
 
 
 if __name__ == "__main__":
-    crawl_megathreads()
+    # a = list()
+    # tools.save_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\\"
+    #                           r"Ukraine_War\all_megathreads_post_ids_temp", a)
+    # crawl_megathreads(start_post_id="uwm88b")
+    a = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Ukraine_War\all_megathreads_post_ids")
+    print()
