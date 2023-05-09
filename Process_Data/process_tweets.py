0cbecae0813a67382b75ba0ccd7f16230d74918f
imoutidi <imoutidi@gmail.com>
2023-05-09 10:55:44 +0200

    Populating the climate change database with tweets and author information.

--- Process_Data/process_tweets.py
+++ Process_Data/process_tweets.py
@@ -5,29 +5,40 @@ import tweepy
 # print(editdistance.eval('one banana', 'banana one'))
 import os
 
-class TextArchiver:
+
+class TweetArchiver:
     def __init__(self):
         self.path = r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\\" \
                           r"Climate_Changed\\"
         self.input_path = self.path + r"Downloaded_Tweets\\"
         self.output_path = self.path + r"I_O\\"
+        self.client = MongoClient('localhost', 27017)
+        self.db = self.client.Climate_Change_Tweets
+        self.collection = self.db.tweet_documents
 
     def parse_tweets(self):
         for folder_index in range(16):
+            print(folder_index)
             for filename in os.listdir(self.input_path + str(folder_index)):
-                tweets = tools.load_pickle(self.input_path + str(folder_index) + r"\\" + filename)
+                tweet_records = tools.load_pickle(self.input_path + str(folder_index) + r"\\" + filename)
+                for tweet_obj in tweet_records:
+                    self.mongo_insert(tweet_obj)
 
-                print()
-
-    def mongo_tester(self):
-        client = MongoClient('localhost', 27017)
-        db = client.Climate_Change_Tweets
-        collection = db.tweet_documents
-        collection.insert_one({"id": 200, "Author": "imoutidi", "Text": "Some tweet text here."})
+    def mongo_insert(self, tweet_object):
+        author_username = tweet_object.author.name
+        author_id = tweet_object.author.id
+        full_text = tweet_object.full_text
+        tweet_id = tweet_object.id
+        tweet_date = tweet_object.created_at
+        try:
+            self.collection.insert_one({"author_id": author_id, "author_username": author_username,
+                                        "full_text": full_text, "tweet_id": tweet_id, "tweet_date": tweet_date})
+        except errors.DuplicateKeyError as key_error:
+            print(key_error)
+            print(tweet_id)
 
 
 if __name__ == "__main__":
-    tweet_archiver = TextArchiver()
-    # network_creator.parse_tweets()
-    tweet_archiver.mongo_tester()
+    climate_change_archiver = TweetArchiver()
+    climate_change_archiver.parse_tweets()
     print()
