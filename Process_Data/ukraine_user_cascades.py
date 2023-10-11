ff4995a3f50f5b1442de75ef61f111357f1deee9
imoutidi <imoutidi@gmail.com>
2023-10-11 16:02:20 +0200

    Creating comment cascades.

--- Process_Data/ukraine_user_cascades.py
+++ Process_Data/ukraine_user_cascades.py
@@ -34,8 +34,8 @@ def create_submission_index():
 def create_comments_index():
     comments_dict = dict()
     all_records = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Crypto_Currency\\"
-                                    r"New_Submissions\merged_submissions")
-
+                                    r"Comments\all_comments")
+    print()
     for current_record, submission_comments in all_records.items():
         sid = current_record
         for post_comment in submission_comments:
@@ -173,11 +173,11 @@ def print_with_phrase_colored(in_str):
 
 def group_opinion_changed_with_parent_child_comments():
     comments_index = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\\"
-                                       r"Ukraine_War\Indexes\new_ukraine_comments_index")
-    submission_index = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Ukraine_War\\"
-                                         r"Indexes\submissions_index")
+                                       r"Crypto_Currency\Indexes\comments_index")
+    submission_index = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\\"
+                                         r"Crypto_Currency\Indexes\submissions_index")
     opinion_changed_comments = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\\"
-                                                 r"Ukraine_War\Annotations\new_kept_comments")
+                                                 r"Crypto_Currency\Annotations\crypto_kept_comments")
     print()
     # populate with parent comments
     populated_with_parents = list()
@@ -195,7 +195,7 @@ def group_opinion_changed_with_parent_child_comments():
         populated_with_parents.append(group_of_comments)
     # populate_with_child_comments
     for comment_group in populated_with_parents:
-        target_comment_id = comment_group[0]["comment_id"]
+        target_comment_id = comment_group[0]["id"]
         for comment_id, comment_meta in comments_index.items():
             if target_comment_id in comment_meta["parent_id"]:
                 comment_meta["comment_id"] = comment_id
@@ -213,16 +213,16 @@ def group_opinion_changed_with_parent_child_comments():
                 comment_group.append(submission)
                 break
     print()
-    tools.save_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Ukraine_War\\"
-                      r"Annotations\new_populated_opinion_change_comments_with_parents_children_and_submission_post",
+    tools.save_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Crypto_Currency\Annotations\\"
+                      r"new_populated_opinion_change_comments_with_parents_children_and_submission_post",
                       populated_with_parents)
 
 
 def write_permalinks():
-    comments = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Ukraine_War\Annotations\\"
-                                 r"For_Opinion_Change_Dataset\only_the_opinion_change_comment")
-    with open(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Ukraine_War\\"
-              r"Annotations\For_Opinion_Change_Dataset\opinion_change_permalinks.html", "w") as out_file:
+    comments = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Crypto_Currency\\"
+                                 r"Annotations\crypto_kept_comments")
+    with open(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Crypto_Currency\Annotations\\"
+              r"For_Opinion_Change_Dataset\opinion_change_permalinks.html", "w") as out_file:
         out_file.write("<!DOCTYPE html>\n<html>\n<head>\n \t <base href=\"https://www.reddit.com/\"/>\n</head>\n<body>\n")
         for idx, comment in enumerate(comments):
             url = comment["permalink"]
@@ -242,27 +242,16 @@ def merge_submissions():
 
 
 def keep_checked_opinion_change_comments():
-    ids_of_checked_comments = [0, 2, 3, 4, 6, 9, 10, 13, 16, 17, 19, 20, 22, 24, 25, 26, 28, 29, 30, 31, 34, 35, 36, 37,
-                               38, 39, 40, 41, 42, 51, 53, 54, 55, 56, 58, 63, 64, 65, 68, 73, 75, 76, 77, 78, 79, 80,
-                               81, 82, 84, 87, 89, 90, 91, 93, 94, 95, 96, 97, 102, 103, 104, 106, 109, 110, 112, 113,
-                               115, 116, 118, 119, 121, 124, 125, 126, 127, 128, 129, 132, 134, 135, 139, 140, 141, 142,
-                               143, 144, 146, 147, 148, 149, 150, 151, 153, 154, 155, 156, 158, 160, 162, 163, 164, 165,
-                               166, 168, 171, 174, 175, 176, 177, 178, 179, 180, 184, 185, 187, 189, 192, 194, 195, 197,
-                               198, 199, 204, 205, 207, 210, 211, 213, 216, 217, 218, 219, 220, 221, 222, 223, 228, 229,
-                               231, 232, 238, 239, 240, 241, 243, 245, 251, 253, 254, 255, 256, 257, 259, 260, 261, 263,
-                               264, 265, 266, 267, 268, 269, 271, 272, 273, 275, 276, 277, 278, 279, 282, 286, 287, 289,
-                               290, 291, 292, 294, 295, 296, 297, 302, 304, 307, 310, 314, 315, 316, 317, 319, 320, 322,
-                               323, 324, 326, 327, 328, 330, 331, 333, 335, 338, 339, 342, 345, 348, 350, 352, 354, 356,
-                               357, 360, 361, 367, 368, 370, 372, 376, 377, 378, 380, 381, 384, 387, 389, 397, 398, 400,
-                               401, 404, 405, 407, 410, 411, 412, 413, 414, 417, 419, 420, 422, 424, 425, 426, 428, 436,
-                               437, 438, 439, 440, 442, 443, 446, 447, 449, 450, 453, 454, 455, 456, 457, 458, 459, 460,
-                               464, 465, 466, 467, 470, 471, 472, 475, 477, 479, 482, 483, 484, 486, 487, 496, 497, 498,
-                               499, 500, 501, 502, 505, 506, 507, 508, 509, 510, 511, 512, 513, 515, 516, 517, 518, 519,
-                               520, 522, 524, 527, 528, 529, 530, 532, 533, 535, 536, 537, 538, 539, 542, 546, 547, 549,
-                               550, 552, 553, 555, 557, 558, 559, 560, 561, 562, 563, 567, 568, 569, 571, 573, 574, 576,
-                               578, 579, 580, 581, 582, 583, 584, 586, 587, 589, 590, 591, 592, 595, 596, 598, 599, 600,
-                               603, 605, 606, 610, 611, 612, 615, 616, 618, 619, 620, 621, 624, 627, 629, 631, 634, 636,
-                               638, 639, 644]
+    ids_of_checked_comments = [1, 2, 4, 14, 17, 19, 20, 21, 25, 28, 29, 31, 33, 35, 41, 45, 49, 51, 62, 63, 66, 67, 78,
+                               86, 94, 96, 100, 101, 102, 103, 106, 107, 108, 110, 112, 113, 119, 121, 123, 125, 126,
+                               128, 133, 134, 135, 138, 139, 140, 141, 146, 148, 150, 152, 153, 156, 158, 160, 162, 165,
+                               168, 170, 174, 176, 177, 178, 181, 183, 185, 193, 196, 200, 203, 211, 224, 253, 256, 270,
+                               299, 306, 307, 309, 311, 314, 325, 326, 327, 328, 331, 332, 334, 341, 359, 361, 364, 366,
+                               367, 372, 374, 380, 381, 382, 387, 392, 402, 403, 412, 418, 437, 458, 459, 463, 467, 471,
+                               480, 489, 568, 576, 582, 586, 597, 602, 627, 653, 654, 661, 667, 668, 676, 685, 689, 693,
+                               698, 710, 717, 728, 732, 734, 739, 744, 755, 779, 781, 788, 789, 791, 795, 797, 798, 799,
+                               808, 820, 821, 824, 825, 842, 845, 847, 853, 855, 856, 857, 859, 860, 861, 862, 863, 865,
+                               866, 867, 868, 873, 874, 875, 876, 878, 881, 882, 889, 890, 891, 893, 899, 913]
     children_parents = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Ukraine_War\\"
                                          r"Annotations\new_populated_opinion_change_comments_with_parents_children_and_submission_post")
     children_parents_small = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Ukraine_War\\"
@@ -284,15 +273,16 @@ def keep_checked_opinion_change_comments():
 
 
 if __name__ == "__main__":
-    create_comments_index()
-    create_submission_index()
+    # create_comments_index()
+    # create_submission_index()
     # merge_submissnions()
     # scan_for_agreement_phrases()
     # annotate()
-    # group_on
-    # pinion_changed_with_parent_child_comments()
-    # write_permalinks()
+    # group_opinion_changed_with_parent_child_comments()
+    write_permalinks()
     # keep_checked_opinion_change_comments()
-    # a = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Crypto_Currency\Comments\all_comments")
-    # b = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Crypto_Currency\Annotations\crypto_kept_comments")
+    # a = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Crypto_Currency\Annotations\\"
+    #                       r"new_populated_opinion_change_comments_with_parents_children_and_submission_post")
+    # b = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Coordination\I_O\Datasets\Crypto_Currency\\
+    # Annotations\crypto_kept_comments")
     print()
